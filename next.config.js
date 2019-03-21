2afccba3320b616f572d585987fefa58253646cc
Kyle Thompson <kyle@kylekthompson.com>
2019-03-20 22:17:56 -0400

    dynamic-nested-pages

--- next.config.js
+++ next.config.js
@@ -1,20 +1,46 @@
-const thoughts = require('./components/thoughts/thoughts');
+const fs = require('fs');
 
-const thoughtPages = thoughts.reduce((pages, thought) => ({
+function flatten(array) {
+  return array.reduce((flattened, currentElement) => {
+    if (Array.isArray(currentElement)) {
+      return [...flattened, ...flatten(currentElement)];
+    }
+
+    return [...flattened, currentElement];
+  }, []);
+}
+
+function expandDirectories(basePath, filesAndDirectories) {
+  return filesAndDirectories.map((fileOrDirectory) => {
+    const relativePathToFileOrDirectory = basePath + '/' + fileOrDirectory;
+    const stats = fs.statSync(relativePathToFileOrDirectory);
+
+    if (stats.isDirectory()) {
+      return findFiles(relativePathToFileOrDirectory);
+    }
+
+    return relativePathToFileOrDirectory;
+  });
+}
+
+function findFiles(path) {
+  const filesAndDirectories = fs.readdirSync(path);
+  const publicFilesAndDirectories = filesAndDirectories.filter((path) => !path.startsWith('_'));
+  const publicFiles = expandDirectories(path, publicFilesAndDirectories);
+  return flatten(publicFiles);
+}
+
+const hrefs = findFiles('./pages')
+  .map((file) => file.endsWith('/index.js') ? file.replace('/index.js', '/') : file.replace('.js', ''))
+  .map((file) => file.replace('./pages', ''));
+
+const pages = hrefs.reduce((pages, href) => ({
   ...pages,
-  [thought.href]: {
-    page: thought.href,
+  [href]: {
+    page: href,
   },
 }), {});
 
 module.exports = {
-  exportPathMap: () => ({
-    ...thoughtPages,
-    '/': {
-      page: '/',
-    },
-    '/history': {
-      page: '/history',
-    },
-  }),
+  exportPathMap: () => pages,
 };
