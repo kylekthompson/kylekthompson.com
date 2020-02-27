3016b2ddea35ea6fac5779f3204226342f4773cd
Kyle Thompson <kyle@kylekthompson.com>
2020-02-26 21:41:17 -0500

    redesign the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  align-items: center;
   display: flex;
+  filter: drop-shadow(0px 4px 75px rgba(0, 0, 0, 0.25));
   flex-direction: column;
   margin: 0 auto;
-  max-width: 720px;
-  padding-top: 30vh;
-  text-align: center;
+  max-width: 800px;
   width: calc(100vw - 40px);
+`;
+
+const Card = styled.div`
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.offWhite};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: -125px auto 0;
+  min-height: 250px;
+  padding: 40px 0;
+  width: 100%;
 `;
 
 const Header = styled.h1`
@@ -20,8 +32,10 @@ export default function NotFound() {
   return (
     <Layout>
       <Container>
-        <Header>Bummer.</Header>
-        <p>There doesn't seem to be anything here.</p>
+        <Card>
+          <Header>404</Header>
+          <p>Uh oh. Can't seem to find that.</p>
+        </Card>
       </Container>
     </Layout>
   );
